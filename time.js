5795f58677bf3bbde595bbd08c6c7f1a82ffe8ed
juno <juno0348@naver.com>
2021-06-30 17:52:21 +0900

    init

---
+++ time.js
@@ -0,0 +1,8 @@
+var year = 2021;
+var born = 2003;
+var old = 19;
+
+born = prompt("태어난 연도를 입력하세요()");
+old = year - born +1;
+document.write(year + "년현재 <br>");
+document.write(born + "년에 태어난 사람의 나이는" + old + "세입니다.");
